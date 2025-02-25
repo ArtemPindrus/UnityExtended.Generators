916fba15237622f77094ec49e1a9a50da99c502b
Artem Pindrus <artempindrus@gmail.com>
2025-02-25 16:49:47 +0200

    Update Program.cs

--- UseGenerator/Program.cs
+++ UseGenerator/Program.cs
@@ -1,23 +1,30 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.IO;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using UnityExtended.Generator;
 
 internal class Program {
     public static void Main() {
         string mocking = File.ReadAllText(@"C:\Users\Artem\Documents\C#\UnityExtended.Generator\UseGenerator\Mocking\MockingUnity.cs");
+        string attributes = File.ReadAllText(@"C:\Users\Artem\Documents\C#\UnityExtended.Generator\UseGenerator\Mocking\ExtendedAttributes.cs");
         string source = File.ReadAllText(@"C:\Users\Artem\Documents\C#\UnityExtended.Generator\UseGenerator\Source.cs");
 
         SyntaxTree sourceSyntaxTree = CSharpSyntaxTree.ParseText(source);
         SyntaxTree mockingSyntaxTree = CSharpSyntaxTree.ParseText(mocking);
-        
+        SyntaxTree attributesSyntaxTree = CSharpSyntaxTree.ParseText(attributes);
+
+        var references = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+
         CSharpCompilation compilation = CSharpCompilation
-            .Create("UseGenerator", [sourceSyntaxTree, mockingSyntaxTree])
-            .AddReferences(MetadataReference.CreateFromFile(typeof(string).Assembly.Location));
+            .Create("UseGenerator", [mockingSyntaxTree, attributesSyntaxTree, sourceSyntaxTree])
+            .AddReferences(
+                references
+            );
 
         var generator = new UnityGenerator();
         
         GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
-        driver.RunGenerators(compilation);
+        var d = driver.RunGenerators(compilation);
     }
 }
 
