338ece022c27550031d3823aea46d5b41c431444
Artem Pindrus <artempindrus@gmail.com>
2025-03-23 13:28:39 +0200

    GetComponentAheadAttribute

--- UnityExtended.Generator/Helpers/AttributesHelper.cs
+++ UnityExtended.Generator/Helpers/AttributesHelper.cs
@@ -3,4 +3,5 @@ namespace UnityExtended.Generator.Helpers;
 public static class AttributesHelper {
     public const string AttributesNamespace = "UnityExtended.Generators.Attributes";
     public const string GetComponentAttribute = $"{AttributesNamespace}.GetComponentAttribute";
+    public const string GetComponentAheadAttribute = $"{AttributesNamespace}.GetComponentAheadAttribute";
 }
