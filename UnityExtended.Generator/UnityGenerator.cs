4d8e92b0b05241fda2c1ee64f0fc83716ce79b7d
Artem Pindrus <artempindrus@gmail.com>
2025-03-23 12:46:42 +0200

    Start migration to new design

--- UnityExtended.Generator/UnityGenerator.cs
+++ UnityExtended.Generator/UnityGenerator.cs
@@ -9,99 +9,31 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using UnityExtended.Generator.ClassFillers;
+using UnityExtended.Generator.Helpers;
 
 namespace UnityExtended.Generator;
 
 [Generator]
 public class UnityGenerator : IIncrementalGenerator {
     public void Initialize(IncrementalGeneratorInitializationContext context) {
-        var getComponentProvider = context.SyntaxProvider.ForAttributeWithMetadataName(
-                $"{GeneratorHelper.AttributesNamespace}.GetComponentAttribute",
-                static (_,_) => true, GetComponentAttributeData.Transform)
-            .WhereNotNullValues()
-            .Collect()
-            .Select((x, _) => GetComponentAttributeData.Assemble(x));
-        
-        var getComponentAheadProvider = context.SyntaxProvider.ForAttributeWithMetadataName(
-                $"{GeneratorHelper.AttributesNamespace}.GetComponentAheadAttribute",
-                static (_,_) => true, GetComponentAheadAttributeData.Transform)
-            .WhereNotNullValues()
-            .Collect()
-            .Select((x, _) => GetComponentAheadAttributeData.Assemble(x));
-        
-        var handleInputProvider = context.SyntaxProvider.ForAttributeWithMetadataName(
-                $"{GeneratorHelper.AttributesNamespace}.HandleInputAttribute",
-                static (_,_) => true, HandleInputAttributeData.TransformToIGenerate)
-            .WhereNotNullValues()
+        var getComponentProvider = context.SyntaxProvider.ForAttributeWithMetadataName(AttributesHelper.GetComponentAttribute,
+            predicate: static (_, _) => true,
+            transform: ClassContextFactory.GetComponentFiller)
             .Collect();
         
-        var collectProvider = context.SyntaxProvider.ForAttributeWithMetadataName(
-                $"{GeneratorHelper.AttributesNamespace}.CollectAttribute",
-                static (_,_) => true, CollectAttributeData.TransformIntoIGenerate)
-            .Collect();
-        
-        var serializePropertyProvider = context.SyntaxProvider.ForAttributeWithMetadataName(
-            $"{GeneratorHelper.AttributesNamespace}.SerializePropertyWithBackingAttribute",
-            static (_,_) => true, SerializePropertyWithBackingAttributeData.TransformIntoIGenerate)
-            .Collect();
-
-        var foldoutGroupProvider = context.SyntaxProvider.ForAttributeWithMetadataName(
-            $"{GeneratorHelper.AttributesNamespace}.StartFoldoutGroupAttribute",
-            static (_, _) => true, FoldoutGroupAttributeData.TransformIntoIGenerate)
-            .Collect();
-        
-        var displayProvider = context.SyntaxProvider.CreateSyntaxProvider(
-                predicate: (n,_) => n is ClassDeclarationSyntax,
-                transform: DisplayAttributeData.Transform)
-            .WhereNotNullValues()
-            .Collect();
-
-        var provider = GeneratorHelper.ValuesCombine(getComponentProvider, 
-            getComponentAheadProvider,
-            handleInputProvider, 
-            collectProvider, 
-            serializePropertyProvider, 
-            foldoutGroupProvider,
-            displayProvider);
-        
-        context.RegisterSourceOutput(provider, Execute);
+        context.RegisterSourceOutput(getComponentProvider, Execute);
     }
     
-    private static void Execute(SourceProductionContext context, ImmutableArray<IGenerate> requiredGeneratedData) {
-        var classes = GeneratorHelper.ExtractGeneratedClassesFromData(requiredGeneratedData);
+    private static void Execute(SourceProductionContext context, ImmutableArray<Class> classes) {
+        var uniqueClasses = classes.Distinct(ClassSignatureEqualityComparer.Default).ToArray();
 
-        AddSecondMethodsIfNecessary(classes);
+        foreach (var c in uniqueClasses) {
+            c.Finish();
+        }
         
-        context.AddSource(classes);
-    }
-    
-    private static void AddSecondMethodsIfNecessary(IEnumerable<Class> classesToGenerate) {
-        foreach (var classToGenerate in classesToGenerate) {
-            Add(classToGenerate, GeneratorHelper.AwakeMethodSignature, GeneratorHelper.Awake2MethodSignature);
-            
-            Add(classToGenerate, GeneratorHelper.OnEnableMethodSignature, GeneratorHelper.OnEnable2MethodSignature);
-            
-            Add(classToGenerate, GeneratorHelper.OnDisableMethodSignature, GeneratorHelper.OnDisable2MethodSignature);
-            
-            Add(classToGenerate, GeneratorHelper.OnValidateMethodSignature, GeneratorHelper.OnValidate2MethodSignature);
-            
-            Add(classToGenerate, GeneratorHelper.OnDestroyMethodSignature, GeneratorHelper.OnDestroy2MethodSignature);
-        }
-
-        static void Add(Class classToGenerate, string existingSignature, string generatedSignature, bool addCallAtTheEnd = true) {
-            Method? existingMethod =
-                classToGenerate.Methods.FirstOrDefault(x => x.Signature == existingSignature);
-            
-            if (existingMethod != default) {
-                var generateMethod = new Method(generatedSignature);
-                classToGenerate.AddMethod(generateMethod);
-
-                if (addCallAtTheEnd) {
-                    var calling = generatedSignature.Substring(generatedSignature.LastIndexOf(' ') + 1) + ";";
-
-                    existingMethod.AddStatement(calling);
-                }
-            }
-        }
+        context.AddSource(uniqueClasses);
+        
+        // TODO: report diagnostics
     }
 }
