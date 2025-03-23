ec384a0d21c48aac8d45ad51ce933b6b4f9b8bd3
Artem Pindrus <artempindrus@gmail.com>
2025-03-23 11:39:25 +0200

    Update Source.cs

--- UseGenerator/Source.cs
+++ UseGenerator/Source.cs
@@ -1,22 +1,20 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+using UnityExtended.Core.Attributes;
 using UnityExtended.Generators.Attributes;
 
+#pragma warning disable
+
 namespace MyNamespace {
-    [Collect]
-    [HandleInput(typeof(MyInput.InteractionActions), typeof(DragAndDropInput.DragAndDropActions))]
-    public partial class Something : MonoBehavior {
-        [field: Display]
-        private TargetAngles TargetAngles { get; set; }
+    public partial class MyClass : MonoBehavior {
+        [GetComponent]
+        private Rigidbody rb;
 
-        [Display]
-        private float x;
-
-        [SerializePropertyWithBacking]
-        private MonoBehavior M {
-            get => new();
-            set => throw new NotSupportedException();
-        }
+        [GetComponent]
+        private Image image;
+        
+        [GetComponent]
+        private float s;
     }
 }
