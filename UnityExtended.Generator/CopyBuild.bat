c2018e0ab4b2c466cd9a97b2db3248cc7511e477
Artem Pindrus <artempindrus@gmail.com>
2025-03-23 12:43:20 +0200

    Create CopyBuild.bat

---
+++ UnityExtended.Generator/CopyBuild.bat
@@ -0,0 +1,3 @@
+set outputPath=C:\Users\Artem\Documents\Unity\Testing\Assets\External\UnityExtended.Core\Generators
+set dllPath=bin\Release\netstandard2.0\UnityExtended.Generator.dll
+copy /y %dllPath% %outputPath%
